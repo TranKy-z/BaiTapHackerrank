6e8100e2cefe20964121af722678ec429cb33697
RenMing <tranminhky109@gmail.com>
2026-01-29 14:04:31 +0700

    .

---
+++ BT C++ - Buoi hoc/Buoi 1/Bai1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+int x1 = 1;
+int y1 = 2;
+int x2 = 3;
+int y2 = 4;
+double d = sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+cout << d;
+return 0;
+}
